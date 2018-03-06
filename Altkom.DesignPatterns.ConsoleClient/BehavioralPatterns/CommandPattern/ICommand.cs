e43588cd54c356d1f15afb75cd146e58535ef8ec
student <student@G7_10>
2018-03-06 16:07:12 +0100

    Dodanie wzorca Command

---
+++ Altkom.DesignPatterns.ConsoleClient/BehavioralPatterns/CommandPattern/ICommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Altkom.DesignPatterns.ConsoleClient.BehavioralPatterns.CommandPattern
+{
+    public interface ICommand
+    {
+        void Execute();
+        bool CanExecute();
+    }
+
+    public class Mail
+    {
+        public string From { get; set; }
+        public string To { get; set; }
+        public string Subject { get; set; }
+        public string Content { get; set; }
+    }
+
+    public class SendCommand : ICommand
+    {
+        private readonly Mail mail;
+
+        public SendCommand(Mail mail)
+        {
+            this.mail = mail;
+        }
+
+        public bool CanExecute()
+        {
+            return
+                   !string.IsNullOrEmpty(mail.From)
+                && !string.IsNullOrEmpty(mail.To);
+        }
+
+        public void Execute()
+        {
+            Console.WriteLine("Sending...");
+        }
+    }
+}
---
+++ Altkom.DesignPatterns.UnitTests/CommandUnitTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Altkom.DesignPatterns.ConsoleClient.BehavioralPatterns.CommandPattern;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Altkom.DesignPatterns.UnitTests
+{
+    [TestClass]
+    public class CommandUnitTest
+    {
+        [TestMethod]
+        public void CommandTest()
+        {
+            Mail mail = new Mail
+            {
+                From = "marcin.sulecki@altkom.pl",
+                To = "marcin.sulecki@altkom.pl"
+            };
+
+            ICommand command = new SendCommand(mail);
+
+            Assert.IsTrue(command.CanExecute());
+
+            command.Execute();
+        }
+
+        [TestMethod]
+        public void CommandNotSendTest()
+        {
+            Mail mail = new Mail
+            {
+                From = "marcin.sulecki@altkom.pl",         
+            };
+
+            ICommand command = new SendCommand(mail);
+
+            Assert.IsFalse(command.CanExecute());
+
+            command.Execute();
+        }
+    }
+}
